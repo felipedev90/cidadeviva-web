de01aeefaa409baeabba7d58be6f8095139d6608
Felipe Augusto <augusto.felipedev@gmail.com>
2026-06-28 16:32:21 -0300

    feat(highlight): adjust layout and styles

--- src/data/serraHighlight.ts
+++ src/data/serraHighlight.ts
@@ -1,14 +1,17 @@
 type SerraData = {
+  id: string
   value: string
   label: string
 }
 
 export const SERRA_DATA: SerraData[] = [
   {
+    id: '1',
     value: '354 km²',
     label: 'de área preservada.',
   },
   {
+    id: '2',
     value: 'Dezenas',
     label: 'de trilhas mapeadas no Strava.',
   },
